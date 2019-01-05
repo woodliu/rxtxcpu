52100a1152de57a77e566a98df79f8a01d2ee2bc
Matthew Cover <matthew.cover@stackpath.com>
2019-01-05 01:28:17 -0700

    Source rvm on every run so that it's always a function, not an executable; this is required for `rvm use` to work on subsequent runs.

--- test.sh
+++ test.sh
@@ -21,10 +21,11 @@ rvm --version 2>/dev/null || {
       --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
                   7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable
-  set +e
-  . "$HOME"/.rvm/scripts/rvm
-  set -e
 }
+
+set +e
+. "$HOME"/.rvm/scripts/rvm
+set -e
 
 ruby_version="2.3.4"
 
