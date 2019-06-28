woodliu/rxtxcpu

commit 5b2842d08ed46093b04792f708640348158b1112

Migrate from manager(), worker() layout to rxtx_open(), rxtx_loop(), rxtx_close() layout.

rxtx.c
@@ -1,0 +1,513 @@
+/*
+ * Copyright (c) 2018-present StackPath, LLC
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * rxtx.c -- multi-stream (per-cpu, per-queue, etc.) packet captures
+ */
+
+#define _GNU_SOURCE
+
+#include "rxtx.h"
+
+#include "ext.h"       // for ext(), noext_copy()
+#include "interface.h" // for interface_set_promisc_on()
+#include "sig.h"       // for keep_running
+
+#include <arpa/inet.h>       // for htons()
+#include <linux/if_packet.h> // for PACKET_FANOUT, PACKET_OUTGOING,
+                             //     PACKET_RX_RING, PACKET_TX_RING,
+                             //     sockaddr_ll, tpacket_req
+#include <net/ethernet.h>    // for ETH_P_ALL
+#include <net/if.h>          // for if_nametoindex()
+#include <sys/socket.h>      // for AF_PACKET, bind(), recvfrom(),
+                             //     setsockopt(), SO_RCVTIMEO, SOCK_RAW,
+                             //     sockaddr, socket(), SOL_PACKET, SOL_SOCKET
+#include <sys/time.h>        // for timeval
+
+#include <errno.h>    // for errno
+#include <pcap.h>     // for bpf_u_int32, DLT_EN10MB, pcap_close(),
+                      //     pcap_dump(), pcap_dump_close(), pcap_dump_flush(),
+                      //     pcap_dump_open(), pcap_geterr(), pcap_open_dead(),
+                      //     pcap_pkthdr
+#include <pthread.h>  // for pthread_mutex_destroy(), pthread_mutex_init(),
+                      //     pthread_mutex_lock(), pthread_mutex_unlock(),
+                      //     pthread_self()
+#include <sched.h>    // for sched_getcpu()
+#include <stdbool.h>  // for true
+#include <stdio.h>    // for fprintf(), NULL, stderr, stdout, sprintf()
+#include <stdlib.h>   // for calloc(), exit(), free()
+#include <string.h>   // for memset(), strcmp(), strerror(), strlen()
+#include <time.h>     // for time()
+#include <unistd.h>   // for getpid()
+
+#define EXIT_FAIL 1
+
+/*
+ * The largest positive number an int can hold is 2,147,483,647. The smallest
+ * negative number an int can hold is -2,147,483,648. Therefore, the longest
+ * strlen() value for string produced by printf() and friends is 11 characters.
+ *
+ *   strlen("-2147483648");
+ */
+#define MAX_INT_AS_STRING_LENGTH 11
+
+#define PACKET_BUFFER_SIZE 65535
+#define SNAPLEN 65535
+
+#define packet_direction_is_rx(sll) (!packet_direction_is_tx(sll))
+#define packet_direction_is_tx(sll) ((sll)->sll_pkttype == PACKET_OUTGOING)
+
+char *program_basename = NULL;
+
+static void rxtx_desc_init(struct rxtx_desc *p, struct rxtx_args *args);
+static void rxtx_desc_destroy(struct rxtx_desc *p);
+static void rxtx_pcap_init(struct rxtx_pcap *p, char *filename, int ring_idx);
+static void rxtx_pcap_destroy(struct rxtx_pcap *p);
+static void
+rxtx_ring_init(struct rxtx_ring *p, struct rxtx_desc *rtd, int ring_idx);
+static void rxtx_ring_destroy(struct rxtx_ring *p);
+static void rxtx_stats_init(struct rxtx_stats *p);
+static void rxtx_stats_destroy(struct rxtx_stats *p);
+static void rxtx_increment_counters(struct rxtx_ring *ring);
+
+static void rxtx_desc_init(struct rxtx_desc *p, struct rxtx_args *args) {
+  p->args = args;
+  p->rings = calloc(args->ring_count, sizeof(*p->rings));
+  p->stats = calloc(1, sizeof(*p->stats));
+  rxtx_stats_init(p->stats);
+  p->ifindex = 0;
+  p->fanout_group_id = 0;
+
+  /*
+   * If we were supplied an ifname, we need to lookup the ifindex. Otherwise,
+   * we want ifindex to be 0 to match any interface.
+   *
+   * When an ifindex lookup is needed, we want the ifindex before entering our
+   * ring init loop and do not yet have a socket fd. It's more clear to create,
+   * use, and discard a temporary socket fd for our SIOCGIFINDEX ioctl request
+   * than to try to use one of the socket fds we'll create later.
+   *
+   * if_nametoindex() does exactly what we want.
+   */
+  if (args->ifname) {
+    p->ifindex = if_nametoindex(args->ifname);
+
+    if (!p->ifindex) {
+      fprintf(
+        stderr,
+        "%s: Failed to get ifindex for interface '%s'\n",
+        program_basename,
+        args->ifname
+      );
+      exit(EXIT_FAIL);
+    }
+  }
+
+  if (args->verbose) {
+    if (p->ifindex == 0) {
+      fprintf(stderr, "Using ifindex '%u' for any interface.\n", p->ifindex);
+    } else {
+      fprintf(
+        stderr,
+        "Using ifindex '%u' for interface '%s'.\n",
+        p->ifindex,
+        args->ifname
+      );
+    }
+  }
+
+  /*
+   * We only have to enable promiscuity once and it will stick around for the
+   * duration of the process.
+   */
+  if (args->promiscuous) {
+    if (p->ifindex == 0) {
+      if (args->verbose) {
+        fprintf(
+          stderr,
+          "Skipping promiscuous mode for ifindex '%u' (any interface).\n",
+          p->ifindex
+        );
+      }
+    } else if (interface_set_promisc_on(p->ifindex) == -1) {
+      fprintf(
+        stderr,
+        "%s: Failed to enable promiscuous mode for ifindex '%u'.\n",
+        program_basename,
+        p->ifindex
+      );
+      exit(EXIT_FAIL);
+    }
+  }
+
+  /*
+   * This loop creates our rings, including per-ring socket fds and pcap
+   * filenames. We need the instantiation order to follow ring index order.
+   *
+   * This needs to happen after we have our ifindex and have enabled
+   * promiscuous mode if requested.
+   */
+  for (int i = 0; i < args->ring_count; i++) {
+    rxtx_ring_init(&(p->rings[i]), p, i);
+  }
+
+  /*
+   * We need an id to add socket fds to our fanout group.
+   */
+  p->fanout_group_id = getpid() & 0xffff;
+
+  if (args->verbose) {
+    fprintf(stderr, "Generated fanout group id '%d'.\n", p->fanout_group_id);
+  }
+}
+
+static void rxtx_desc_destroy(struct rxtx_desc *p) {
+  p->fanout_group_id = 0;
+  p->ifindex = 0;
+  rxtx_stats_destroy(p->stats);
+  free(p->stats);
+  p->stats = NULL;
+  /*
+   * We need to destroy our rings in reverse order. The lowest ring owns the
+   * pcap struct instance when writing to stdout. We don't want to free that
+   * memory too soon as other rings need to access struct members.
+   */
+  for (int i = p->args->ring_count - 1; i < 0; i--) {
+    rxtx_ring_destroy(&(p->rings[i]));
+  }
+  free(p->rings);
+  p->rings = NULL;
+  p->args = NULL;
+}
+
+static void rxtx_pcap_init(struct rxtx_pcap *p, char *filename, int ring_idx) {
+  if (filename) {
+    if (strcmp(filename, "-") == 0) {
+      p->filename = filename;
+    } else {
+      p->filename = calloc(
+        1,
+        strlen(filename) + MAX_INT_AS_STRING_LENGTH + 2 /* for "-" and '\0' */
+      );
+      char *copy = noext_copy(filename);
+      sprintf(
+        p->filename,
+        "%s-%d.%s",
+        copy,
+        ring_idx,
+        ext(filename)
+      );
+      free(copy);
+    }
+
+    p->desc = pcap_open_dead(DLT_EN10MB, SNAPLEN);
+
+    if ((p->fp = pcap_dump_open(p->desc, p->filename)) == NULL) {
+      fprintf(
+        stderr,
+        "%s: Error opening dump file '%s' for writing: %s\n",
+        program_basename,
+        p->filename,
+        pcap_geterr(p->desc)
+      );
+      exit(EXIT_FAIL);
+    }
+
+    p->mutex = calloc(1, sizeof(*p->mutex));
+    if (pthread_mutex_init(p->mutex, NULL) != 0) {
+      fprintf(
+        stderr,
+        "%s: Error initializing mutex for dump file '%s'.\n",
+        program_basename,
+        p->filename
+      );
+      exit(EXIT_FAIL);
+    }
+
+    p->owner_idx = ring_idx;
+  }
+}
+
+static void rxtx_pcap_destroy(struct rxtx_pcap *p) {
+  if (p->filename) {
+    pthread_mutex_destroy(p->mutex);
+    free(p->mutex);
+    p->mutex = NULL;
+
+    /* protect against silent write failures */
+    if ((pcap_dump_flush(p->fp)) == -1) {
+      fprintf(
+        stderr,
+        "%s: Error writing to dump file '%s'.\n",
+        program_basename,
+        p->filename
+      );
+      exit(EXIT_FAIL);
+    }
+
+    pcap_dump_close(p->fp);
+    p->fp = NULL;
+    pcap_close(p->desc);
+    p->desc = NULL;
+    if (strcmp(p->filename, "-") != 0) {
+      free(p->filename);
+    }
+    p->filename = NULL;
+    p->owner_idx = 0;
+  }
+}
+
+static void
+rxtx_ring_init(struct rxtx_ring *p, struct rxtx_desc *rtd, int ring_idx) {
+  p->rtd = rtd;
+  p->stats = calloc(1, sizeof(*p->stats));
+  rxtx_stats_init(p->stats);
+  p->idx = ring_idx;
+  p->fd = -1;
+
+  if (rtd->args->fanout_mode != NO_PACKET_FANOUT) {
+    /*
+     * The AF_PACKET address family gives us a packet socket at layer 2. The
+     * SOCK_RAW socket type gives us packets which include the layer 2 header.
+     * The htons(ETH_P_ALL) protocol gives us all packets from any protocol.
+     */
+    p->fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (p->fd == -1) {
+      fprintf(
+        stderr,
+        "%s: Failed to create socket: %s\n",
+        program_basename,
+        strerror(errno)
+      );
+      exit(EXIT_FAIL);
+    }
+
+    /*
+     * We're using the default, TPACKET_V1, presently. Updating to TPACKET_V3
+     * would be cool.
+     *
+     * PACKET_RX_RING sets up a mmapped ring buffer for async packet reception.
+     * PACKET_TX_RING sets up a mmapped ring buffer for packet transmission.
+     *
+     * I'm not sure if packets marked with PACKET_OUTGOING end up in the rx
+     * ring buffer or the tx ring buffer. PACKET_TX_RING may only be used for
+     * packets sent by this application, but it doesn't seem to cost much to
+     * set it up.
+     */
+    struct tpacket_req req;
+    memset(&req, 0, sizeof(req));
+    setsockopt(p->fd, SOL_PACKET, PACKET_RX_RING, (void *)&req, sizeof(req));
+    setsockopt(p->fd, SOL_PACKET, PACKET_TX_RING, (void *)&req, sizeof(req));
+
+    /*
+     * SO_RCVTIMEO sets a timeout for socket i/o system calls like recvfrom().
+     * This gives us an easy way to keep our worker loops from getting stuck
+     * waiting on a packet which may never arrive.
+     */
+    struct timeval receive_timeout;
+    /* no need for memset(), we're initializing every member */
+    receive_timeout.tv_sec = 0;
+    receive_timeout.tv_usec = 10;
+    setsockopt(
+      p->fd,
+      SOL_SOCKET,
+      SO_RCVTIMEO,
+      &receive_timeout,
+      sizeof(receive_timeout)
+    );
+
+    /*
+     * Per packet(7), we need to set sll_family, sll_protocol, and sll_ifindex
+     * in the sockaddr_ll we're passing to bind(). The values for sll_family
+     * and sll_protocol are straight out of our earlier call to socket(). The
+     * value for sll_ifindex is from our lookup.
+     */
+    struct sockaddr_ll sll;
+    memset(&sll, 0, sizeof(sll));
+    sll.sll_family = AF_PACKET;
+    sll.sll_protocol = htons(ETH_P_ALL);
+    sll.sll_ifindex = rtd->ifindex;
+
+    if (bind(p->fd, (struct sockaddr *)&sll, sizeof(sll)) == -1) {
+      fprintf(stderr, "%s: Failed to bind socket.\n", program_basename);
+      exit(EXIT_FAIL);
+    }
+
+    /*
+     * Add the socket to our fanout group using the fanout mode supplied.
+     */
+    int fanout_arg = (rtd->fanout_group_id | (rtd->args->fanout_mode << 16));
+    if (setsockopt(
+          p->fd,
+          SOL_PACKET,
+          PACKET_FANOUT,
+          &fanout_arg,
+          sizeof(fanout_arg)
+        ) < 0) {
+      fprintf(stderr, "%s: Failed to configure fanout.\n", program_basename);
+      exit(EXIT_FAIL);
+    }
+
+    /*
+     * TODO: Should we be calling shutdown() on sockets for rings not in our
+     *       ring_set?
+     *
+     *         int shutdown (int socket, int how)
+     */
+  }
+
+  /*
+   * We need to init our pcaps after we've successfully opened sockets.
+   * Otherwise the pcap header gets written to stdout even when socket setup
+   * fails.
+   */
+  if (rtd->args->pcap_filename &&
+      strcmp(rtd->args->pcap_filename, "-") == 0) {
+    if (ring_idx == 0) {
+      p->pcap = calloc(1, sizeof(*p->pcap));
+      rxtx_pcap_init(p->pcap, rtd->args->pcap_filename, ring_idx);
+    } else {
+      p->pcap = rtd->rings[0].pcap;
+    }
+  } else {
+    p->pcap = calloc(1, sizeof(*p->pcap));
+    rxtx_pcap_init(p->pcap, rtd->args->pcap_filename, ring_idx);
+  }
+}
+
+static void rxtx_ring_destroy(struct rxtx_ring *p) {
+  /*
+   * TODO: Should we be calling close() on sockets?
+   *
+   *         int close (int filedes)
+   */
+  p->fd = 0;
+  rxtx_stats_destroy(p->stats);
+  free(p->stats);
+  p->stats = NULL;
+  p->rtd = NULL;
+  if (p->pcap->owner_idx == p->idx) {
+    rxtx_pcap_destroy(p->pcap);
+    free(p->pcap);
+  }
+  p->pcap = NULL;
+  p->idx = 0;
+}
+
+static void rxtx_stats_init(struct rxtx_stats *p) {
+  p->packets_received = 0;
+  p->mutex = calloc(1, sizeof(*p->mutex));
+  if (pthread_mutex_init(p->mutex, NULL) != 0) {
+    fprintf(
+      stderr,
+      "%s: Error initializing mutex for stats.\n",
+      program_basename
+    );
+    exit(EXIT_FAIL);
+  }
+}
+
+static void rxtx_stats_destroy(struct rxtx_stats *p) {
+  pthread_mutex_destroy(p->mutex);
+  free(p->mutex);
+  p->mutex = NULL;
+  p->packets_received = 0;
+}
+
+static void rxtx_increment_counters(struct rxtx_ring *ring) {
+  pthread_mutex_lock(ring->rtd->stats->mutex);
+  ring->rtd->stats->packets_received++;
+  pthread_mutex_unlock(ring->rtd->stats->mutex);
+  ring->stats->packets_received++;
+}
+
+int rxtx_open(struct rxtx_desc *rtd, struct rxtx_args *args) {
+  rxtx_desc_init(rtd, args);
+  return 0;
+}
+
+int rxtx_close(struct rxtx_desc *rtd) {
+  rxtx_desc_destroy(rtd);
+  return 0;
+}
+
+void *rxtx_loop(void *r) {
+  struct rxtx_ring *ring = r;
+  struct rxtx_desc *rtd = ring->rtd;
+  struct rxtx_pcap *pcap = ring->pcap;
+  struct rxtx_args *args = rtd->args;
+
+  if (args->verbose) {
+    fprintf(
+      stderr,
+      "Worker '%lu' handling ring '%d' running on cpu '%d'.\n",
+      pthread_self(),
+      ring->idx,
+      sched_getcpu()
+    );
+  }
+
+  while (keep_running) {
+
+    if (args->packet_count &&
+        rtd->stats->packets_received >= args->packet_count) {
+      break;
+    }
+
+    struct sockaddr_ll sll;
+    unsigned char packet_buffer[PACKET_BUFFER_SIZE];
+
+    unsigned int sll_length = sizeof(sll);
+    int packet_length = recvfrom(
+      ring->fd,
+      packet_buffer,
+      sizeof(packet_buffer),
+      0,
+      (struct sockaddr *)&sll,
+      &sll_length
+    );
+
+    if (packet_length == -1) {
+      continue;
+    }
+
+    if (!args->capture_rx && packet_direction_is_rx(&sll)) {
+      continue;
+    }
+
+    if (!args->capture_tx && packet_direction_is_tx(&sll)) {
+      continue;
+    }
+
+    rxtx_increment_counters(ring);
+
+    if (pcap->filename) {
+      /* no need for memset(), we're initializing every member */
+      struct pcap_pkthdr pcap_packet_header;
+      pcap_packet_header.caplen     = (bpf_u_int32)packet_length;
+      pcap_packet_header.len        = (bpf_u_int32)packet_length;
+      pcap_packet_header.ts.tv_sec  = time(NULL);
+      pcap_packet_header.ts.tv_usec = 0;
+
+      pthread_mutex_lock(pcap->mutex);
+      pcap_dump((unsigned char *)pcap->fp, &pcap_packet_header, packet_buffer);
+
+      if (args->packet_buffered) {
+        if ((pcap_dump_flush(pcap->fp)) == -1) {
+          fprintf(
+            stderr,
+            "%s: Error writing to dump file '%s'.\n",
+            program_basename,
+            pcap->filename
+          );
+          exit(EXIT_FAIL);
+        }
+      }
+      pthread_mutex_unlock(pcap->mutex);
+    }
+  }
+  return NULL;
+}

Makefile
@@ -10,13 +10,13 @@ rxtxcpu.o: EXTRA_CFLAGS = \
 	-std=c99 \
 	'-DRXTXCPU_VERSION="$(RXTXCPU_VERSION)"'
 
-cpu.o manager.o: EXTRA_CFLAGS = \
+cpu.o rxtx.o: EXTRA_CFLAGS = \
 	-std=c99
 
 %.o: %.c
 	$(CC) $(CFLAGS) $(EXTRA_CFLAGS) -c -o $@ $<
 
-rxtxcpu rxcpu txcpu: cpu.o ext.o interface.o manager.o rxtxcpu.o sig.o worker.o
+rxtxcpu rxcpu txcpu: cpu.o ext.o interface.o rxtx.o rxtxcpu.o sig.o
 	$(CC) $(CFLAGS) -o rxtxcpu $^ -lpcap -lpthread
 	rm -f rxcpu txcpu
 	ln -s rxtxcpu rxcpu
@@ -24,7 +24,7 @@ rxtxcpu rxcpu txcpu: cpu.o ext.o interface.o manager.o rxtxcpu.o sig.o worker.o
 
 .PHONY: clean
 clean:
-	rm -f cpu.o ext.o interface.o manager.o rxtxcpu.o sig.o worker.o rxtxcpu rxcpu txcpu
+	rm -f cpu.o ext.o interface.o rxtx.o rxtxcpu.o sig.o rxtxcpu rxcpu txcpu
 
 .PHONY: install
 install: rxtxcpu rxcpu txcpu

manager.c
@@ -1,293 +1,0 @@
-/*
- * Copyright (c) 2018-present StackPath, LLC
- * All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * manager.c -- setup sockets, spin up workers, and print results
- */
-
-#define _GNU_SOURCE          // for CPU_SET(), cpu_set_t, CPU_ZERO()
-
-#include "ext.h"             // for ext(), noext_copy()
-#include "interface.h"       // for interface_set_promisc_on()
-#include "manager.h"         // for manager_arguments
-#include "sig.h"             // for setup_signals()
-#include "worker.h"          // for total_packets_received, worker_arguments
-
-#include <arpa/inet.h>       // for htons()
-#include <linux/if_packet.h> // for PACKET_FANOUT, PACKET_FANOUT_CPU,
-                             //     PACKET_RX_RING, PACKET_TX_RING, sockaddr_ll,
-                             //     tpacket_req
-#include <net/ethernet.h>    // for ETH_P_ALL
-#include <net/if.h>          // for if_nametoindex()
-#include <sys/socket.h>      // for AF_PACKET, bind(), setsockopt(),
-                             //     SO_RCVTIMEO, SOCK_RAW, sockaddr, socket(),
-                             //     SOL_PACKET, SOL_SOCKET
-#include <sys/time.h>        // for timeval
-#include <errno.h>           // for errno
-#include <pthread.h>         // for pthread_attr_destroy(),
-                             //     pthread_attr_init(),
-                             //     pthread_attr_setaffinity_np(),
-                             //     pthread_attr_t, pthread_create(),
-                             //     pthread_join(), pthread_t
-#include <sched.h>           // for CPU_SET(), cpu_set_t, CPU_ZERO()
-#include <stdio.h>           // for FILE, fprintf(), NULL, stderr, sprintf()
-#include <stdlib.h>          // for malloc()
-#include <string.h>          // for memset(), strcmp(), strerror(), strlen()
-#include <unistd.h>          // for getpid()
-
-#define RETURN_BAD -1
-#define RETURN_GOOD 0
-
-/*
- * The largest positive number an int can hold is 2,147,483,647. The smallest
- * negative number an int can hold is -2,147,483,648. Therefore, the longest
- * strlen() value for string produced by printf() and friends is 11 characters.
- *
- *   strlen("-2147483648");
- */
-#define MAX_INT_AS_STRING_LENGTH 11
-
-int manager(struct manager_arguments *args) {
-
-  /*
-   * If we were supplied an ifname, we need to lookup the ifindex. Otherwise,
-   * we want ifindex to be 0 to match any interface.
-   *
-   * When an ifindex lookup is needed, we want the ifindex before entering our
-   * loop and do not yet have a socket fd. It's more clear to create, use, and
-   * discard a temporary socket fd for our SIOCGIFINDEX ioctl request than to
-   * try to use one of the socket fds we'll create in our loop.
-   * if_nametoindex() does exactly what we want.
-   */
-  unsigned int ifindex;
-  if (args->ifname) {
-    ifindex = if_nametoindex(args->ifname);
-
-    if (!ifindex) {
-      fprintf(stderr, "%s: Failed to get ifindex for interface '%s'\n", args->program_basename, args->ifname);
-      return RETURN_BAD;
-    }
-  } else {
-    ifindex = 0;
-  }
-
-  if (args->verbose) {
-    if (ifindex == 0) {
-      fprintf(stderr, "Using ifindex '%u' for any interface.\n", ifindex);
-    } else {
-      fprintf(stderr, "Using ifindex '%u' for interface '%s'.\n", ifindex, args->ifname);
-    }
-  }
-
-  /*
-   * We only have to enable promiscuity once and it will stick around for the
-   * duration of the process.
-   */
-  if (args->promiscuous) {
-    if (ifindex == 0) {
-      if (args->verbose) {
-        fprintf(stderr, "Skipping promiscuous mode for ifindex '%u' (any interface).\n", ifindex);
-      }
-    } else if (interface_set_promisc_on(ifindex) == -1) {
-      fprintf(stderr, "%s: Failed to enable promiscuous mode for ifindex '%u'.\n", args->program_basename, ifindex);
-      return RETURN_BAD;
-    }
-  }
-
-  /*
-   * We need an id to add socket fds to our fanout group.
-   */
-  int fanout_group_id = getpid() & 0xffff;
-
-  if (args->verbose) {
-    fprintf(stderr, "Generated fanout group id '%d'.\n", fanout_group_id);
-  }
-
-  /*
-   * This loop primarily creates our socket fds. We need the instantiation order indexed
-   * by processor id.
-   *
-   * Secondarily, it creates our per-processor pcap filenames.
-   */
-  struct worker_arguments worker_args[args->processor_count];
-  pthread_t worker_threads[args->processor_count];
-
-  for (int i = 0; i < args->processor_count; i++) {
-    /*
-     * The AF_PACKET address family gives us a packet socket at layer 2. The
-     * SOCK_RAW socket type gives us packets which include the layer 2 header.
-     * The htons(ETH_P_ALL) protocol gives us all packets from any protocol.
-     */
-    int socket_fd;
-    socket_fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    if (socket_fd == -1) {
-      fprintf(stderr, "%s: Failed to create socket: %s\n", args->program_basename, strerror(errno));
-      return RETURN_BAD;
-    }
-
-    /*
-     * We're using the default, TPACKET_V1, presently. Updating to TPACKET_V3
-     * would be cool.
-     *
-     * PACKET_RX_RING sets up a mmapped ring buffer for async packet reception.
-     * PACKET_TX_RING sets up a mmapped ring buffer for packet transmission.
-     *
-     * I'm not sure if packets marked with PACKET_OUTGOING end up in the rx
-     * ring buffer or the tx ring buffer. PACKET_TX_RING may only be used for
-     * packets sent by this application, but it doesn't seem to cost much to
-     * set it up.
-     */
-    struct tpacket_req req;
-    memset(&req, 0, sizeof(req));
-    setsockopt(socket_fd, SOL_PACKET, PACKET_RX_RING, (void *)&req, sizeof(req));
-    setsockopt(socket_fd, SOL_PACKET, PACKET_TX_RING, (void *)&req, sizeof(req));
-
-    /*
-     * SO_RCVTIMEO sets a timeout for socket i/o system calls like recvfrom().
-     * This gives us an easy way to keep our worker loops from getting stuck
-     * waiting on a packet which may never arrive.
-     */
-    struct timeval receive_timeout;
-    /* no need for memset(), we're initializing every member */
-    receive_timeout.tv_sec = 0;
-    receive_timeout.tv_usec = 10;
-    setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &receive_timeout, sizeof(receive_timeout));
-
-    /*
-     * Per packet(7), we need to set sll_family, sll_protocol, and sll_ifindex
-     * in the sockaddr_ll we're passing to bind(). The values for sll_family
-     * and sll_protocol are straight out of our earlier call to socket(). The
-     * value for sll_ifindex is from our lookup.
-     */
-    struct sockaddr_ll sll;
-    memset(&sll, 0, sizeof(sll));
-    sll.sll_family = AF_PACKET;
-    sll.sll_protocol = htons(ETH_P_ALL);
-    sll.sll_ifindex = ifindex;
-
-    if (bind(socket_fd, (struct sockaddr *)&sll, sizeof(sll)) == -1) {
-      fprintf(stderr, "%s: Failed to bind socket.\n", args->program_basename);
-      return RETURN_BAD;
-    }
-
-    /*
-     * Per packet(7), "PACKET_FANOUT_CPU selects the socket based on the CPU
-     * that the packet arrived on."
-     *
-     * The implementation in fanout_demux_cpu() (net/packet/af_packet.c) is a
-     * simple modulo operaton.
-     *
-     *   smp_processor_id() % num;
-     *
-     * num is the number of sockets in the fanout group. The sockets are
-     * indexed in the same order as they were added to the fanout group. So, we
-     * simply need one socket per processor added to the fanout group in
-     * processor id order.
-     */
-    int fanout_arg = (fanout_group_id | (PACKET_FANOUT_CPU << 16));
-    if (setsockopt(
-          socket_fd,
-          SOL_PACKET,
-          PACKET_FANOUT,
-          &fanout_arg,
-          sizeof(fanout_arg)
-        ) < 0) {
-      fprintf(stderr, "%s: Failed to configure fanout.\n", args->program_basename);
-      return RETURN_BAD;
-    }
-
-    worker_args[i].socket_fd = socket_fd;
-
-    if (!CPU_ISSET(i, &(args->capture_cpu_set))) {
-      continue;
-    }
-
-    if (args->pcap_filename) {
-      if (strcmp(args->pcap_filename, "-") == 0) {
-        worker_args[i].pcap_filename = args->pcap_filename;
-      } else {
-        worker_args[i].pcap_filename = malloc(
-          strlen(args->pcap_filename)
-          + MAX_INT_AS_STRING_LENGTH
-          + 2  /* for - and \0 */
-        );
-        char *copy = noext_copy(args->pcap_filename);
-        sprintf(
-          worker_args[i].pcap_filename,
-          "%s-%d.%s",
-          copy,
-          i,
-          ext((char *)args->pcap_filename)
-        );
-        free(copy);
-      }
-    } else {
-      /*
-       * args->pcap_filename is NULL.
-       */
-      worker_args[i].pcap_filename = NULL;
-    }
-
-    worker_args[i].program_basename = args->program_basename;
-    worker_args[i].packet_count     = args->packet_count;
-    worker_args[i].capture_rx       = args->capture_rx;
-    worker_args[i].capture_tx       = args->capture_tx;
-    worker_args[i].packet_buffered  = args->packet_buffered;
-    worker_args[i].verbose          = args->verbose;
-  }
-
-  /*
-   * Setup our signal handlers before spinning up workers.
-   */
-  setup_signals();
-
-  /*
-   * This loop spins up our workers. Each worker is affine to a single
-   * processor and is passed the socket fd which will receive packets for that
-   * processor.
-   */
-  cpu_set_t cpu_set;
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
-
-  for (int i = 0; i < args->processor_count; i++) {
-    if (!CPU_ISSET(i, &(args->capture_cpu_set))) {
-      continue;
-    }
-    CPU_ZERO(&cpu_set);
-    CPU_SET(i, &cpu_set);
-    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set), &cpu_set);
-    pthread_create(&worker_threads[i], &attr, worker, (void *)&worker_args[i]);
-  }
-
-  /*
-   * This loop joins our workers and prints the per-worker results.
-   */
-  FILE *out = stdout;
-  if (args->pcap_filename &&
-      strcmp(args->pcap_filename, "-") == 0) {
-    out = stderr;
-  }
-  for (int i = 0; i < args->processor_count; i++) {
-    if (!CPU_ISSET(i, &(args->capture_cpu_set))) {
-      continue;
-    }
-    pthread_join(worker_threads[i], NULL);
-    fprintf(
-      out,
-      "%ju packets captured on cpu%d.\n",
-      worker_args[i].results.worker_packets_received,
-      i
-    );
-    free(worker_args[i].pcap_filename);
-  }
-
-  pthread_attr_destroy(&attr);
-
-  fprintf(out, "%ju packets captured total.\n", total_packets_received);
-
-  return RETURN_GOOD;
-}

manager.h
@@ -1,35 +1,0 @@
-/*
- * Copyright (c) 2018-present StackPath, LLC
- * All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * manager.h -- header file to use manager.c
- */
-
-#ifndef _MANAGER_H_
-#define _MANAGER_H_
-
-#include <sched.h>   // for cpu_set_t
-#include <stdbool.h> // for bool
-#include <stdint.h>  // for uintmax_t
-
-struct manager_arguments {
-  char      *ifname;
-  char      *pcap_filename;
-  char      *program_fullname;
-  char      *program_basename;
-  uintmax_t packet_count;
-  int       processor_count;
-  cpu_set_t capture_cpu_set;
-  bool      capture_rx;
-  bool      capture_tx;
-  bool      packet_buffered;
-  bool      promiscuous;
-  bool      verbose;
-};
-
-int manager(struct manager_arguments *args);
-
-#endif // _MANAGER_H_

commit c5ce159193e99e833755453a888b937ada61d188

Fix extraneous pcap files.
5b2842d introduced an issue causing pcap files to be created for all rings,
even those which are disabled.

Fix this and add a test to prevent regression.

features/write.feature
@@ -37,3 +37,8 @@ Feature: `--write=FILE`
     When I run `sudo timeout -s INT 2 bash -c '../../rxtxcpu -l 0 -w - -U lo | tcpdump -c 3 -r -'` in background
     And I run `ping -i0.2 -c3 localhost` on cpu 0
     Then the output from "sudo timeout -s INT 2 bash -c '../../rxtxcpu -l 0 -w - -U lo | tcpdump -c 3 -r -'" should contain "IP localhost > localhost: ICMP echo request"
+
+  Scenario: With `-w out.pcap` on a subset of cpus.
+    When I run `sudo timeout -s INT 2 ../../rxtxcpu -l0 -w out.pcap -U lo`
+    Then a file named "out-0.pcap" should exist
+    And a file named "out-1.pcap" should not exist

rxtx.c
@@ -61,6 +61,8 @@ static void rxtx_pcap_init(struct rxtx_pcap *p, char *filename, int ring_idx);
 static void rxtx_pcap_destroy(struct rxtx_pcap *p);
 static void
 rxtx_ring_init(struct rxtx_ring *p, struct rxtx_desc *rtd, int ring_idx);
+static void
+rxtx_ring_pcap_init(struct rxtx_ring *p, struct rxtx_desc *rtd, int ring_idx);
 static void rxtx_ring_destroy(struct rxtx_ring *p);
 static void rxtx_stats_init(struct rxtx_stats *p);
 static void rxtx_stats_destroy(struct rxtx_stats *p);
@@ -154,6 +156,10 @@ static void rxtx_desc_init(struct rxtx_desc *p, struct rxtx_args *args) {
    */
   for (int i = 0; i < args->ring_count; i++) {
     rxtx_ring_init(&(p->rings[i]), p, i);
+    if (!CPU_ISSET(i, &(args->ring_set))) {
+      continue;
+    }
+    rxtx_ring_pcap_init(&(p->rings[i]), p, i);
   }
 
   /*
@@ -374,7 +380,10 @@ rxtx_ring_init(struct rxtx_ring *p, struct rxtx_desc *rtd, int ring_idx) {
      *         int shutdown (int socket, int how)
      */
   }
+}
 
+static void
+rxtx_ring_pcap_init(struct rxtx_ring *p, struct rxtx_desc *rtd, int ring_idx) {
   /*
    * We need to init our pcaps after we've successfully opened sockets.
    * Otherwise the pcap header gets written to stdout even when socket setup